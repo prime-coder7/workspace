fbee7ba97852e4e9316ea95d0164e2fd62374285
prime-coder7 <shivshaktisweets123@gmail.com>
2024-12-05 16:52:40 +0530

    add

---
+++ Shutil Module/main.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+os.chdir("F:\\workspace\\Shutil Module")
+
+shutil.copy("hello.txt", "hello2.txt")
+
+shutil.copytree("F:\\workspace\\Notepad", "Notepad2")
+
---
+++ req.py
@@ -0,0 +1,5 @@
+import requests
+
+response = requests.get("https://www.codewithharry.com")
+
+print(response.text)
