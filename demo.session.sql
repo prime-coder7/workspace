007db416293d65fe71fcda79c64a14fc5a7d2ed1
prime-coder7 <shivshaktisweets123@gmail.com>
2024-12-14 21:50:18 +0530

    add

---
+++ demo.session.sql
@@ -0,0 +1,18 @@
+-- create table student (id INTEGER primary key, name varchar(20), city varchar(20));
+
+--
+
+INSERT INTO student (id, name, city) VALUES
+(1, 'Aarav', 'Mumbai'),
+(2, 'Vivaan', 'Delhi'),
+(3, 'Aditya', 'Bengaluru'),
+(4, 'Sai', 'Hyderabad'),
+(5, 'Ishaan', 'Chennai'),
+(6, 'Reyansh', 'Kolkata'),
+(7, 'Ayaan', 'Pune'),
+(8, 'Krishna', 'Jaipur'),
+(9, 'Ravi', 'Lucknow'),
+(10, 'Arjun', 'Ahmedabad');
+
+
+
